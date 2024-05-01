fef957cdda4bb2601b5ad504fd72ec862f5fff06
Jingchao Di <alswlx@gmail.com>
2024-05-01 21:19:15 +0800

    feat: enable editor

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from "umi";
+import { defineConfig } from 'umi';
 
 export default defineConfig({
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
-  ],
+  routes: [{ path: '/', component: 'index' }],
   npmClient: 'pnpm',
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,46 @@
-import yayJpg from '../assets/yay.jpg';
+import { Col, Row, theme } from 'antd';
+import MonacoEditor from 'react-monaco-editor';
 
-export default function HomePage() {
+export default () => {
+  // constructor
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  const code = `Table staff {
+  id int [pk]
+  name varchar
+  age int
+  email varchar
+}`;
+
+  // editorDidMount
+  const editorDidMount = (editor: any, monaco: any) => {
+    console.log('editorDidMount', editor);
+  };
+
+  // onchange
+  const onChange = (newValue: any, e: any) => {
+    console.log('onChange', newValue, e);
+  };
+
   return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-    </div>
+    <Row>
+      <Col span={12}>
+        <MonacoEditor
+          width="800"
+          height="600"
+          // dbml not works
+          language="dbml"
+          theme="vs-dark"
+          value={code}
+          options={{
+            selectOnLineNumbers: true,
+          }}
+          onChange={onChange}
+          editorDidMount={editorDidMount}
+        />
+      </Col>
+      <Col span={12}></Col>
+    </Row>
   );
-}
+};
